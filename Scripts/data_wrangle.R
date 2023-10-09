e2dcb17676ac4702c54279d8f8667603636239b7
Sahin <ssahin@mail.wlu.edu>
2023-10-09 16:38:39 -0400

    changes to script

--- Scripts/data_wrangle.R
+++ Scripts/data_wrangle.R
@@ -1,1 +1,7 @@
-list <- list.files('~/Project/covid19-state-policy/Data/csse_covid_19_daily_reports_us')
+time_series_list <- list.files('~/Project/covid19-state-policy/Data/csse_covid_19_daily_reports_us', full.names = TRUE)
+
+df <- lapply(time_series_list, function(file){
+  read.csv(file)
+})
+
+binded_df <- do.call(rbind, df)
