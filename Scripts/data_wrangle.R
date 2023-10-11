f8512d6538f5ec79c3ab583e1a4bb9acbc01995a
Sahin <ssahin@mail.wlu.edu>
2023-10-11 15:42:51 -0400

    Tons of Pre-Processing Script

--- Scripts/data_wrangle.R
+++ Scripts/data_wrangle.R
@@ -1,7 +1,27 @@
+library(lubridate)
+library(tidyverse)
+
 time_series_list <- list.files('~/Project/covid19-state-policy/Data/csse_covid_19_daily_reports_us', full.names = TRUE)
 
+date_list <- list.files('~/Project/covid19-state-policy/Data/csse_covid_19_daily_reports_us')
+
+
 df <- lapply(time_series_list, function(file){
-  read.csv(file)
+  table <- read.csv(file)[,1:18]
+  table$Date <- substring(file, 90,99) 
+  
+  table
 })
 
 binded_df <- do.call(rbind, df)
+
+binded_df <- filter(binded_df, !(binded_df$Province_State %in% c("American Samoa",
+                                                             "Diamond Princess",
+                                                             "Grand Princess",
+                                                             "Guam",
+                                                             "Northern Mariana Islands",
+                                                             "Puerto Rico",
+                                                             "Virgin Islands")))
+
+#sort from least recent to most recent 
+sorted_df <- binded_df %>% arrange(mdy(binded_df$Date))
