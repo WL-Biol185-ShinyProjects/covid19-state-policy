181de194425bac0af40873f38378c67f4dc5ac69
Sahin <ssahin@mail.wlu.edu>
2023-10-11 15:51:20 -0400

    Changes to file path

--- Scripts/data_wrangle.R
+++ Scripts/data_wrangle.R
@@ -1,10 +1,7 @@
 library(lubridate)
 library(tidyverse)
 
-time_series_list <- list.files('~/Project/covid19-state-policy/Data/csse_covid_19_daily_reports_us', full.names = TRUE)
-
-date_list <- list.files('~/Project/covid19-state-policy/Data/csse_covid_19_daily_reports_us')
-
+time_series_list <- list.files('Data/csse_covid_19_daily_reports_us', full.names = TRUE)
 
 df <- lapply(time_series_list, function(file){
   table <- read.csv(file)[,1:18]
