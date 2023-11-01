7d86c505b1d097acc9e727c3d130ed3e00b4aeb2
Sahin <ssahin@mail.wlu.edu>
2023-11-01 16:02:13 -0400

    Cleaned out negative death values, increased functionality to first page

--- Scripts/App/server.R
+++ Scripts/App/server.R
@@ -1,5 +1,6 @@
 library(ggplot2)
 library(tidyverse)
+library(zoo)
 
 # Define server 
 file1 <- "../../Data/covid19_state_policy_tidydata.csv"
@@ -16,8 +17,8 @@ function(input, output, session) {
   
   output$indexPlot <- renderPlot({
   
-    indexPlotTitle <- paste(as.character(input$y), "Over Time — ", as.character(input$State))
-    
+    indexPlotTitle <- paste(as.character(input$Index), "Over Time — ", as.character(input$State))
+
     policyData %>%
       filter(Province_State == input$State) %>%
       ggplot(aes_string(input$x, input$Index, group = 1)) +
@@ -43,7 +44,7 @@ function(input, output, session) {
                         "dailyDeaths", 
                         group = 1)) +
       geom_point(color='red', fill = 'red', size=1, alpha = 0.7) +
-      geom_smooth(color = 'blue') +
+      geom_smooth(method = "loess", color = 'blue', span = .25) +
       xlim(as.Date("2020-04-12"), as.Date("2022-12-31"))+
       scale_x_date(date_labels="%b %d %Y", 
                    breaks = as.Date(c("2020-04-12", "2020-07-01", 
