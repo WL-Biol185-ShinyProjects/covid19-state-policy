718659983493cf4d8fc6b7c1d71578844c459734
Sahin <ssahin@mail.wlu.edu>
2023-11-24 12:36:42 -0500

    Implementation of conditional panel

--- Scripts/App/ui.r
+++ Scripts/App/ui.r
@@ -99,8 +99,7 @@ ui <- fluidPage(
                      inputId = "State",
                      label = "Province_State:",
                      choices = c(unique(policyData$Province_State)),
-                     selected = "Alabama"
-                   )
+                     selected = "Alabama")
                    ),
                  
                  # Output: Show plots
@@ -116,71 +115,87 @@ ui <- fluidPage(
                      # CONSIDER USING CONDITONAL PANEL AND WELL PANEL FOR AESTHETIC AND USER INTERFACE
                      
                      wellPanel(
-                       
-                      sidebarLayout(
-                      # Sidebar to demonstrate slider option for time
-                        sidebarPanel(
-
-                        # Input: Simple time interval
-                          sliderInput("Time", "Time:",
-                                      min = as.Date("2020-04-13"), 
-                                      max = as.Date("2022-12-31"),
-                                      value = as.Date("2020-04-13"), 
-                                      animate = animationOptions(interval = 250, loop = TRUE)),
-                          # Input: Select Index
-                          selectInput(
-                            inputId = "IndexTab2",
-                            label = "Index",
-                            choices = c("StringencyIndex", "ContainmentHealthIndex", "GovernmentResponseIndex", "EconomicSupportIndex"),
-                            selected = "StringencyIndex"),
-
-                          # Input: Select Low Density States
-                          selectInput(
-                            multiple = TRUE,
-                            inputId = "lowDensityStates",
-                            label = "Low Density States",
-                            choices = lowDensityStates,
-                            selected = lowDensityStates),
-
-                          # Input: Select Medium Density States
-                          selectInput(
-                            multiple = TRUE,
-                            inputId = "mediumDensityStates",
-                            label = "Medium Density States",
-                            choices = mediumDensityStates,
-                            selected = mediumDensityStates),
-
-                          # Input: Select High Density States
-                          selectInput(
-                            multiple = TRUE,
-                            inputId = "highDensityStates",
-                            label = "High Density States",
-                            choices = highDensityStates,
-                            selected = highDensityStates)),
-                        
-                        mainPanel("Plot2",
-                                  fluidRow(
-                                    splitLayout(cellWidths = c("50%", "50%"),
-                                                       plotlyOutput(outputId = 'DeathsOverTimebyDensityLow',
-                                                                    width = "240px",
-                                                                    height = "480px"),
-                                                       plotlyOutput('IndexOverTimeLow'))),
-                                  br(),
-                                  br(),
-                                  fluidRow(
-                                    splitLayout(cellWidths = c("50%", "50%"),
-                                                plotlyOutput(outputId = 'DeathsOverTimebyDensityMedium',
-                                                             width = "240px",
-                                                             height = "480px"),
-                                                plotlyOutput('IndexOverTimeMedium'))),
-                                  br(),
-                                  br(),
-                                  fluidRow(
-                                    splitLayout(cellWidths = c("50%", "50%"),
-                                                plotlyOutput('DeathsOverTimebyDensityHigh'),
-                                                plotlyOutput('IndexOverTimeHigh'))))
-        )
-      )
-    )
+                       sidebarLayout(
+                         # Sidebar to demonstrate slider option for time
+                         sidebarPanel(
+                           checkboxGroupInput("plotType", "Density Categories:",
+                                      c("Low Density", "Medium Density", "High Density")
+                                      ),
+                           
+                           # Input: Simple time interval
+                           sliderInput("Time", "Time:",
+                                       min = as.Date("2020-04-13"),
+                                       max = as.Date("2022-12-31"),
+                                       value = as.Date("2020-04-13"),
+                                       animate = animationOptions(interval = 250, loop = TRUE)),
+                           
+                           # Input: Select Index
+                           selectInput(
+                             inputId = "IndexTab2",
+                             label = "Index",
+                             choices = c("StringencyIndex", "ContainmentHealthIndex", "GovernmentResponseIndex", "EconomicSupportIndex"),
+                             selected = "StringencyIndex"),
+                           
+                           # Only show this panel if the plot type is a histogram
+                           conditionalPanel(
+                             condition = "input.plotType.includes('Low Density')",
+                             
+                             # Input: Select Low Density States
+                             selectInput(
+                               multiple = TRUE,
+                               inputId = "lowDensityStates",
+                               label = "Low Density States",
+                               choices = lowDensityStates,
+                               selected = lowDensityStates)),
+                           
+                           # Only show this panel if the plot type is a histogram
+                           conditionalPanel(
+                             condition = "input.plotType.includes('Medium Density')",
+                           
+                             # Input: Select Medium Density States
+                             selectInput(
+                               multiple = TRUE,
+                               inputId = "mediumDensityStates",
+                               label = "Medium Density States",
+                               choices = mediumDensityStates,
+                               selected = mediumDensityStates)),
+                           
+                           # Only show this panel if the plot type is a histogram
+                           conditionalPanel(
+                             condition = "input.plotType.includes('High Density')",
+                             
+                             # Input: Select High Density States
+                             selectInput(
+                               multiple = TRUE,
+                               inputId = "highDensityStates",
+                               label = "High Density States",
+                               choices = highDensityStates,
+                               selected = highDensityStates))),
+                           
+                           mainPanel("Plot2",
+                                     fluidRow(
+                                       splitLayout(cellWidths = c("50%", "50%"),
+                                                   plotlyOutput(outputId = 'DeathsOverTimebyDensityLow',
+                                                                width = "240px",
+                                                                height = "480px"),
+                                                   plotlyOutput('IndexOverTimeLow'))),
+                                     br(),
+                                     br(),
+                                     fluidRow(
+                                       splitLayout(cellWidths = c("50%", "50%"),
+                                                   plotlyOutput(outputId = 'DeathsOverTimebyDensityMedium',
+                                                                width = "240px",
+                                                                height = "480px"),
+                                                   plotlyOutput('IndexOverTimeMedium'))),
+                                     br(),
+                                     br(),
+                                     fluidRow(
+                                       splitLayout(cellWidths = c("50%", "50%"),
+                                                   plotlyOutput('DeathsOverTimebyDensityHigh'),
+                                                   plotlyOutput('IndexOverTimeHigh')))
+                           )
+                         )
+                       )
+                     )
+            )
   )
-)
