dd279c2c7d523dae6ad03eb7c5de9515566538c0
Sahin <ssahin@mail.wlu.edu>
2023-12-14 22:12:48 -0500

    Addition of appendix as well as further descriptions to my other tabs.

--- ui.r
+++ ui.r
@@ -139,8 +139,16 @@ ui <- fluidPage(theme = shinytheme("darkly"),
                      
                      wellPanel(
                        sidebarLayout(
+                         
+                         
                          # Sidebar to demonstrate slider option for time
                          sidebarPanel(
+                           p("These linked plots enables users to examine
+                     trends in state policy response and mortalities
+                     from a cross-section perspective. The states are 
+                           categorized by density. The user may select
+                           the specific states and index to visualize 
+                           from the options below."),
                            checkboxGroupInput("plotType", "Density Categories:",
                                       c("Low Density", "Medium Density", "High Density"),
                                       selected = "Low Density"
@@ -237,7 +245,10 @@ ui <- fluidPage(theme = shinytheme("darkly"),
                                  selected = "C1M_School.closing"
                                ),
                                
-                               p(""),
+                               p("The first visualization ranks US states by the duration of time spent at > 2 for a given OxCGRT policy measure.
+                       Thus, states at the bottom of the table may be considered 'Most Restrictive', while states towards the top may
+                       be considered 'Least Restrictive' in policy response. Users may toggle between the various policy measures above."),
+                               
                                plotOutput("policyMeasureStackedPlot",
                                           height = "640px"),
                                p("Source: Oxford Covid-19 Government Response Tracker", align = "center"),
@@ -248,7 +259,8 @@ ui <- fluidPage(theme = shinytheme("darkly"),
                                  choices = c(lowDensityStates, mediumDensityStates, highDensityStates),
                                  selected = "Alaska"
                                ),
-                               p(""),
+                               p("The second visualization provides a state-specific summary of the various OxCGRT policy measures
+                               across the data collection period. Users may select the state to be visualized using the dropdown above."),
                                plotOutput("policyMeasureStatePlot",
                                           height = "640px"),
                                br(),
@@ -286,7 +298,7 @@ ui <- fluidPage(theme = shinytheme("darkly"),
                                  selected = "Alaska"
                                ),
                                p("The second visualization provides a state-specific summary of the 4 OxCGRT policy indeces (range 0-100)
-                               across the data collection period. Users may select the state to be visualized with the dropdown above."),
+                               across the data collection period. Users may select the state to be visualized using the dropdown above."),
                                plotOutput("indexStatePlot",
                                           height = "640px"),
                                br(),
@@ -360,7 +372,10 @@ ui <- fluidPage(theme = shinytheme("darkly"),
                          "predictOutput",
                          label = h5("Variable to Predict:"),
                          choices = c("dailyDeaths")
-                       )
+                       ),
+                       p("This visualization helps users explore the potential linear regression relationships
+                         between state policy responses and mortality outcomes for the given period
+                         of data collection.")
                        ),
                      mainPanel(
                          h4("Linear Regression"),
@@ -397,6 +412,34 @@ ui <- fluidPage(theme = shinytheme("darkly"),
                                br(),
                                leafletOutput("indexMap")
                                )
+                     ),
+            tabPanel("Appendix",
+                     h2("Appendix"),
+                     br(),
+                     h4("The 4 Oxford Covid-19 Government Reponse Tracker Policy Indeces:"),
+                     em("1. StringencyIndex:"),
+                     p("Documents the severity of closure and containment measures implemented
+                     that restricted population movement and behavior."),
+                     em("2. ContainmentHealthIndex:"),
+                     p("Record the extent and intensity of efforts to contain Covid-19 
+                   and protect public health encompassing closure and containment measures
+                   along with health protocols like Covid-19 testing rate and contact tracing)."),
+                     em("3. GovernmentResponseIndex:"),
+                     p("Reports the variability in state policy response across all 
+                     documented measures."),
+                     em("4. EconomicSupportIndex:"),
+                     p("Reflects the degree economic support made available
+                     to a specific state at a point in time, including (but
+                     not limited to) income assistance and debt forgiveness."),
+                     br(),
+                     h4("Index Methodology"),
+                     p("Users interested in further details of the index methodology may read below:"),
+                     a("OxCGRT Index Methodology", href = "https://github.com/OxCGRT/covid-policy-tracker/blob/master/documentation/index_methodology.md"),
+                     br(),
+                     br(),
+                     h4("Accessing the Raw Data"),
+                     p("Users interested in accessing the raw data from the OxCGRT may do so here:"),
+                     a("OxCGRT - United States - Sub-National Data", href = "https://github.com/OxCGRT/covid-policy-tracker/tree/master/data/United%20States")
                      )
             )
   )
